623827782b0026d4bdb53fdf69845392acd39192
Diego Cruz <diegocruz.c@gmail.com>
2018-02-17 05:44:21 -0500

    Initial Commit

---
+++ itp-mobilelab-week4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  itp-mobilelab-week4
+//
+//  Created by Diego Cruz on 2/17/18.
+//  Copyright © 2018 Diego Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
